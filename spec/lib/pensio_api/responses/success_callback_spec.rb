6b431132f1de67039e7e9e4facbba1c9f1785057
Rory Sinclair <rory@mungler.com>
2014-01-16 16:37:48 +0000

    success example

--- spec/lib/pensio_api/responses/success_callback_spec.rb
+++ spec/lib/pensio_api/responses/success_callback_spec.rb
@@ -4,6 +4,10 @@ describe PensioAPI::Responses::SuccessCallback do
 
   let(:response) { PensioAPI::Callback.parse_success(file_fixture("success_callback.xml")) }
 
+  it "succeeds" do
+    expect(response.success?).to be_true
+  end
+  
   describe ".reservation" do
     it "gets a reservation transaction" do
       expect(response.reservation).to be_an_instance_of(PensioAPI::Transaction)
