c395517c8bd8538124394cdeb5055d2a99413a77
Rory Sinclair <rory@mungler.com>
2014-01-20 14:43:40 +0000

    break examples out to be more granular

--- spec/lib/pensio_api/responses/failure_callback_spec.rb
+++ spec/lib/pensio_api/responses/failure_callback_spec.rb
@@ -11,17 +11,29 @@ describe PensioAPI::Responses::FailureCallback do
   describe ".reservation" do
     it "gets a reservation transaction" do
       expect(response.reservation).to be_an_instance_of(PensioAPI::Transaction)
+    end
+    it "is not reserved" do
       expect(response.reservation).to_not be_reserved
+    end
+    it "attempted to reserve a non-zero amount" do
       expect(response.reservation.reserved_amount).to be > 0.0
+    end
+    it "does not capture any amount" do
       expect(response.reservation.captured_amount).to eq 0.0
     end
   end
   
   describe ".charge" do
-    it "gets a charge transaction without capture" do
+    it "gets a charge transaction" do
       expect(response.charge).to be_an_instance_of(PensioAPI::Transaction)
+    end
+    it "is not captured" do
       expect(response.reservation).to_not be_captured
+    end
+    it "does not reserve any amount" do
       expect(response.charge.reserved_amount).to eq 0.0
+    end
+    it "does not capture any amount" do
       expect(response.charge.captured_amount).to eq 0.0
     end
   end
--- spec/lib/pensio_api/responses/success_callback_spec.rb
+++ spec/lib/pensio_api/responses/success_callback_spec.rb
@@ -11,19 +11,33 @@ describe PensioAPI::Responses::SuccessCallback do
   describe ".reservation" do
     it "gets a reservation transaction" do
       expect(response.reservation).to be_an_instance_of(PensioAPI::Transaction)
+    end
+    it "is reserved" do
       expect(response.reservation).to be_reserved
+    end
+    it "reserves a non-zero amount" do
       expect(response.reservation.reserved_amount).to be > 0.0
+    end
+    it "does not capture any amount" do
       expect(response.reservation.captured_amount).to eq 0.0
     end
   end
   
   describe ".charge" do
-    it "gets a charge transaction with capture" do
+    it "gets a charge transaction" do
       expect(response.charge).to be_an_instance_of(PensioAPI::Transaction)
+    end
+    it "is captured" do
       expect(response.charge).to be_captured
-      expect(response.charge.reserved_amount).to eq(response.charge.captured_amount)
+    end
+    it "reserves a non-zero amount" do
       expect(response.charge.reserved_amount).to be > 0.0
+    end
+    it "captures a non-zero amount" do
       expect(response.charge.captured_amount).to be > 0.0
+    end
+    it "captures the same amount as was reserved" do
+      expect(response.charge.reserved_amount).to eq(response.charge.captured_amount)
     end
   end
   
