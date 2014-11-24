c58c0267e514d2655b999e1274c5428fb3a69907
Rory Sinclair <rory@mungler.com>
2014-11-24 17:06:40 +0000

    fix ReservationRelease

--- lib/pensio_api/responses/reservation_release.rb
+++ lib/pensio_api/responses/reservation_release.rb
@@ -1,7 +1,15 @@
 module PensioAPI
   module Responses
     class ReservationRelease < Base
-      
+      attr_reader :transaction
+
+      def initialize(request)
+        super(request)
+
+        @transaction = PensioAPI::Transaction.new(
+          transactions['Transaction']
+        )
+      end
     end
   end
 end
