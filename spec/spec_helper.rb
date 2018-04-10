690c0ac159108ed8c0709510a697be6c2d21cdcc
sz-altapay-com <sz@altapay.com>
2018-04-10 16:04:50 +0200

    VESPLG-527 Fix the path issue from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'webmock/rspec'
 require 'httparty'
 require 'active_support'
 require 'pensio_api'
-require './spec/support/helpers'
+require_relative './support/helpers'
 
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
