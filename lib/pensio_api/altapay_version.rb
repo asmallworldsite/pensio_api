3b8f256f5b1f3c674e83b236b8202c6d63c2f8dd
sz-altapay-com <sz@altapay.com>
2018-04-11 10:44:02 +0200

    Set a class from where the SDK version can be fetched (this commit comes in addition to the commit: 6101fdd5bac36c70494caf8e72c3518f6e6d429e)

---
+++ lib/pensio_api/altapay_version.rb
@@ -0,0 +1,10 @@
+module PensioAPI
+
+    class AltapayVersion
+
+        def self.getVersion
+        #add here the latest version (typically will be the one that will be released after pushing the latest changes)
+            version = '0.3.4'
+        end
+    end
+end
